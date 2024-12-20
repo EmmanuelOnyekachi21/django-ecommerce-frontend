9f4d18b489e577ade62f864f323cd46c1dbdce74
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2024-12-20 19:29:12 +0100

    dependenies

---
+++ VioletStores/src/components/product/ProductPage.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import ProductPageHolder from './ProductPageHolder'
+
+const ProductPage = () => {
+  return (
+    <div>
+        <ProductPageHolder />
+        <section className="py-3">
+        <div className="container px-4 px-lg-5 my-5">
+            <div className="row gx-4 lg-5 align-items-center">
+                <div className="col-md-6">
+                    <img src="https://dummyimage.com/600x700/dee2e6/6c757d.jpg" alt="..." className="card-img-top mb-5 mb-md-0" />
+                </div>
+                <div className="col-md-6">
+                    <div className="small mb-1">SKU: BST-498</div>
+                    <h1 className="display-5 fw-bolder">HP ELITEBOOK 1030</h1>
+                    <div className="fs-5 mb-5">
+                        <span>$300</span>
+                    </div>
+
+                    <p className="lead">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Praesentium at dolorem quidem modi. Nam sequi consequatur
+                obcaecati excepturi alias magni, accusamus eius blanditiis
+                delectus ipsam minima ea iste laborum vero?
+                    </p>
+                    <div className="d-flex">
+                        <button className="btn btn-outline-dark flex-shrink-0" type='button'>
+                            <i className="bi-cart-fill me-1"></i>
+                            Add to cart
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    </div>
+  )
+}
+
+export default ProductPage
